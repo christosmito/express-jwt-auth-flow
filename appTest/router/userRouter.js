49098f6ee2ec2222b9496f35bf176dd39a47d0bb
christos goulas <christosglx@hotmail.com>
2021-04-24 18:04:18 +0300

    Email now send with sendgrid/email package insteadof nodemailer

--- appTest/router/userRouter.js
+++ appTest/router/userRouter.js
@@ -8,7 +8,14 @@ const router = express.Router();
 router.post("/signup", auth(User).signup);
 router.post("/login", auth(User).login);
 router.post("/update-password", auth(User).updatePassword);
-router.post("/forgot-password", auth(User).forgotPassword);
+router.post("/forgot-password", auth(User, {
+    apiKey: "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",
+    to: "gouc53@gmail.com",
+    from: "christosglx@hotmail.com",
+    subject: "Reset token",
+    text: "This is a test",
+    html: "<h1>This is a test</h1>"
+}).forgotPassword);
 router.post("/reset-password/:token", auth(User).resetPassword);
 
 module.exports = router;
